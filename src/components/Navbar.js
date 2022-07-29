7d3889474d376cf80ca76050b24cbef3972e0bd2
ios10 <trikshotzz05@gmail.com>
2022-07-29 14:44:49 -0700

    more for nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
   return (
@@ -6,7 +8,7 @@ const Navbar = () => {
       <div className="container">
   <a className="navbar__logo" href="#">Ivan</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+   <FontAwesomeIcon icon={faBars} style={{color: "#ffffff"}} />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
