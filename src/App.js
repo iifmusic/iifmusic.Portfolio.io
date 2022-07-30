aa00402cd2ccc6fd9b8579ee76214c301f1b9eec
ios10 <trikshotzz05@gmail.com>
2022-07-30 02:23:28 -0700

    workin gon header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+import './App.css'
+import Particles from 'react-tsparticles'
+// import { loadFull } from "tsparticles";
 
-export default class App extends Component {
-  render() {
+function App() {
+
+    // const particlesInit = async (main) => {
+    //   console.log(main);
+  
+    //   // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    //   // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    //   // starting from v2 you can add only the features you need reducing the bundle size
+    //   await loadFull(main);
+    // };
+  
+    // const particlesLoaded = (container) => {
+    //   console.log(container);
+    // };
     return (
-      
-      <Navbar/>
+      <>
+       <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true, 
+                value_area: 900
+              }
+            }
+          }
+        }}
+       />
+        <Navbar/>
+        <Header />
+      </>
     )
   }
-}
+
+  export default App
