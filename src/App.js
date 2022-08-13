370fab02d480c14c593e613b8c6d5d46ada735e5
Ivan <iif@Anti-2.local>
2022-08-13 13:52:00 -0700

    added dev icons and the about me in progress

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar'
 import Header from './components/Header'
-import './App.css'
+import './App.scss'
 import Particle from './components/Particle'
 import { AboutMe } from './pages/AboutMe'
 import Zoom from 'react-reveal/Zoom'
+import { Languages } from './pages/Languages'
 
 
 
 function App() {
     return (
       <>
-    
-      <Particle />
-      <Navbar/>
-      <Header />
-      <Zoom>
-        <AboutMe />
-      </Zoom>
+        <Particle />    
+        <Navbar/>
+        <Header />
+      <div className='second__area'>
+        <Zoom>
+          <AboutMe />
+        </Zoom>
+        <Languages />
+      </div>
       
       
         
