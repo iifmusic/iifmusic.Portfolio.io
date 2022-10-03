36b5fb68493613abb764cf9243097cd97f9a714a
Ivan <iif@Anti-2.local>
2022-10-03 00:32:37 -0700

    fixing color issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light bg-dark">
+    <nav className="navbar navbar-expand-lg">
       <div className="container">
   <a className="navbar__logo" href="#">Ivan</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
