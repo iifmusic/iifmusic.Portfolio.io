59ede420946ab34eb8e56dd585eaf65e676c8dd8
Ivan <iif@anti.home>
2023-03-06 19:22:31 -0800

    finishing look fo about me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import image from '/Users/iif/Code/Portfolio/src/assets/drink.jpeg'
-import Fade from 'react-reveal/Fade';
-import Zoom from 'react-reveal/Zoom';
+import React from 'react';
+import image from '/Users/iif/Code/Portfolio/src/assets/drink.jpeg';
 import { useEffect, useState } from 'react';
+import { Flip, Zoom, Fade } from 'react-reveal';
 export const AboutMe = () => {
 
 
 
   return (
     <div id='about__me'>
-      <Zoom>
-        <h1>AboutMe</h1>
+      <Zoom duration={2500} top cascade>
+        <h1>About Me</h1>
       </Zoom>
-      <Fade>
-        <p>
-        Who am I? 
-        <br/>
-        Hi, I am Ivan Ortega. I am a self taught, DIY(do it yourself) enthusiast with a passion for music, code, and everything in between. I have studied many kinds of languages for code and I am always asking for a challenge. I thrive in a fast workspace and do so with the least amount of stress and the greatest amount of enthusiasm. Aside from my coding experience I have worked in the hospitality industry for 11+ years, so I am quite astonishing dealing with ordeals down to the smallest dilemmas. I as well produce my own music. I have released 3+ songs on all the music platforms and am working on many more in the meantime. Having learned and self taught most of these skills, I hope to apply both code and music in all my projects to achieve something unique and stylish. You can checkout my <a href='#'>Github</a> or my <a href='#'>music</a>.
-            
-              <div className='about__img'>
+      <Fade delay={2000} duration={4000}>
+        <div className='about__inner'>
+          <div className='about__para'>
+            <p>
+              <div id="who"> WHO AM I? </div>
+                <br/>
+                <br/>
+                Hi there, I'm Ivan Ortega. I am a self taught, DIY(do it yourself) enthusiast with a passion for music, code, and everything in between. I have studied many kinds of code languages and I am always asking for a challenge. I thrive in a fast workspace and do so with the least amount of stress and the greatest amount of enthusiasm (also because I don't like when it's slow... I get way too bored). Aside from my coding experience I have worked in the hospitality industry for 11+ years, so I am quite astonishing dealing with ordeals down to the smallest dilemmas. I as well produce my own music. I have released 3+ songs on all the music platforms and am working on many more in the meantime. Having learned and self taught most of these skills, I hope to apply both code and music in all my projects to achieve something unique and stylish. You can checkout my <a href='https://github.com/iiAnti'>Github</a> or my <a href='https://linktr.ee/iif_music'>Music</a>.
+            </p>
+          </div>
+          <div className='about__img'>
+            <Flip right delay={4000} duration={1000}>
               <img src={ image } />
-              </div>
-            
-
-        </p>
+            </Flip>
+          </div>
+        </div>
         </Fade>
     </div>
   )
