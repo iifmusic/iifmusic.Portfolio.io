6821707bc9272645e260f9c57acf4f61c54278fd
Ivan <ivan_ortega@rocketmail.com>
2023-06-30 00:41:59 -0700

    finsihed simple part of certification

--- src/pages/Certification.js
+++ src/pages/Certification.js
@@ -4,11 +4,13 @@ import image from '/Users/iif/Code/Portfolio/src/assets/certificate.png'
 export const Certification = () => {
   return (
     <>
-        <h2 id='certification__header'>Certifications</h2>
-        <div>
+      <div id='certification'>
+          <h2>Certifications</h2>
+          <div>
 
-            <img src={ image } alt='Google IT Certificaiton'/>
-            <a href='https://www.credly.com/badges/90891639-516e-4320-bc78-d0e3ae502cac'>  </a>
+              <img src={ image } alt='Google IT Certificaiton'/>
+              <a href='https://www.credly.com/badges/90891639-516e-4320-bc78-d0e3ae502cac'> Google IT Certification Verification</a>
+          </div>
         </div>
     </>
   )
