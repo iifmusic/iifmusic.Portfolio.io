936170f6013ebc841dfc57f0a1fd2eeb6b2f2be6
Ivan <ivan_ortega@rocketmail.com>
2023-07-06 13:22:00 -0700

    added a footer to the page for refernece right now

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='footer__container'>
+        <div> iiAnti dist |</div>
+       <a className='footer__back' href='#home'> Back To top</a>
+    </div>
+  )
+}
