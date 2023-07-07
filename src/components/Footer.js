e2b0809ba7d16df6c777ee82336b7882d5fff20e
Ivan <ivan_ortega@rocketmail.com>
2023-07-07 12:25:48 -0700

    added images and links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import iif from '/Users/iif/Code/Portfolio/src/assets/iifBlank.png'
+import facebook from '/Users/iif/Code/Portfolio/src/assets/facebook.png'
+import tiktok from '/Users/iif/Code/Portfolio/src/assets/tiktok.png'
+import instagram from '/Users/iif/Code/Portfolio/src/assets/instagram.png'
+import twitter from '/Users/iif/Code/Portfolio/src/assets/twitter.png'
+
+
+const socialMediaLinks = [
+  { name: 'TikTok', url: 'https://www.tiktok.com/@iifmusic', icon: tiktok  },
+  { name: 'Facebook', url: 'https://www.facebook.com/iif.officialmusic', icon: facebook },
+  { name: 'Instagram', url: 'https://www.instagram.com/iif_officialmusic/', icon: instagram },
+  { name: 'Twitter', url: 'https://twitter.com/iif_officialmsc', icon: twitter },
+];
+
+const iconStyle = {
+  width: '24px',
+  height: '24px',
+  marginRight: '8px',
+};
 
 export const Footer = () => {
   return (
-    <div className='footer__container'>
-        <div> iiAnti dist |</div>
-       <a className='footer__back' href='#home'> Back To top</a>
+    <div id='footer__container'>
+      <img src={iif} alt="iif"/>
+      <footer className="footer">
+        &copy; 2023 iif. All rights reserved | 
+        {socialMediaLinks.map((link) => (
+        <a href={link.url} key={link.name}>
+        <img src={link.icon} alt={link.name} style={iconStyle} />        </a>
+      ))}
+        <a className='footer__back' href='#home'> Back To top</a>
+      </footer>
     </div>
   )
 }
